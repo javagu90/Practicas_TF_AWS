b326ee693af82b58390df29778f86d0637b36300
javagu90 <javagu90@gmail.com>
2021-10-11 00:57:39 -0500

    first commit

---
+++ my_subnet.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region = "us-west-1"
+}
+
+#resource "aws_instance" "test" {
+#  ami = "ami-054965c6cd7c6e462"
+#  instance_type = "t2.micro"
+#}
+
+#creando VPC
+
+resource "aws_vpc" "main"{
+  cidr_block="10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags ={
+    Name ="vpc1"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block="10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-west-1b"
+  tags={
+    Name = "subnet1 en us-west-1b" 
+  }  
+}
